50ecb8664e6907fe429f6757b34e143580545c51
Marcelo Foss <marcelo.foss.rj@gmail.com>
2020-06-19 17:12:34 -0300

    first commit

---
+++ docker-create-volumes.sh
@@ -0,0 +1,4 @@
+docker volume create --name SET_A_NAME-db
+docker volume create --name SET_A_NAME-static_files
+docker volume create --name SET_A_NAME-redis
+docker volume create --name SET_A_NAME-gemfile
